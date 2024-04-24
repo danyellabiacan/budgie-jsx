c6695bbc873eea02366986c25d3aaadcb637ec84
Danyella <danyellabiacan@gmail.com>
2024-04-23 19:25:42 -0700

    changed the landing page background

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -5,7 +5,7 @@ import Login from '../Forms/Login.jsx';
 
 const Landing = () => {
     return (
-        <div className="flex h-dvh w-dvw bg-gradient-to-br from-teal-500 to-cyan-950 items-center justify-center">
+        <div className="flex h-dvh w-dvw bg-gradient-to-br from-lime-200 to-cyan-800 items-center justify-center">
             <div className="flex flex-col space-y-16 size-fit rounded-lg drop-shadow-lg bg-white/50 p-10
                 md:flex-row md:space-y-0 md:space-x-16">
                 <section className="">
